b3ffd8b8c260015fd91f8615bf4f6570e1a6eead
Андрей Репринцев <Randrew8313@gmail.com>
2023-08-02 23:41:27 +0300

    Ex003_sum-update code

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 100); //1 2 3 4 5 ... 99
+Console.Write("Первое число ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.Write("Второе число ");
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.Write("Сумма чисел = ");
 Console.WriteLine(result);
