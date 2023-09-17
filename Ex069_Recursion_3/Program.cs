224ffe74cfacecdc76538ccbb6748b0232e963bf
Андрей Репринцев <Randrew8313@gmail.com>
2023-09-17 12:46:14 +0300

    добавлены задачи по рекурсии

---
+++ Ex069_Recursion_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 69: Напишите задачу, которая на вход принимает два числа А и В, и возводит число А в целую степень В с помощью рекурсии.
+
+double Pow(double num, double rank)
+{
+    if(rank < 0)
+    {
+        return 1 / (num * Pow(num, rank + 1));
+    }
+
+    if(rank == 0)
+    {
+        return 1;
+    }
+
+    if(rank == 1)
+    {
+       return num;
+    }
+
+   return num = num * Pow(num, rank - 1);
+}
+
+Console.Write(Pow(2, -1));
---
+++ Ex67_Recursion_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int GetSum(int num)
+{
+    if(num == 0)
+    {
+        return num;
+    }
+    return num % 10 + GetSum(num / 10);
+}
+
+Console.Write(GetSum(453));
